3464ffc5dfb7c1f5ab4d18e9a089056ecaa368e0
CodeMaster114514 <128393278+CodeMaster114514@users.noreply.github.com>
2023-06-27 22:08:33 +0800

    Save mound.h.

--- src/mound.h
+++ src/mound.h
@@ -1,6 +1,7 @@
 typedef struct{
     void* buffer;
     size_t len;
+    size_t place;
     size_t count;
     size_t write_p;
 } mound;
@@ -11,3 +12,5 @@ void* read(mound* this,size_t p);
 void push(mound* this,void* data);
 void* pop(mound* this);
 void set(mound* this,size_t p,void* data);
+size_t get_count(mound* this);
+void free_mound(mound* this);
