ecd989d21823a5c4e68d14082ed69748784864a5
CodeMaster114514 <128393278+CodeMaster114514@users.noreply.github.com>
2023-06-27 19:46:11 +0800

    Save main.c.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "compiler.h"
 
-int main(){
+int main(int argc,char* args){
+    if(argc < 3){
+        goto error;
+    }
 //	printf("hello world!\n");
 	int res = compile_file("./test.c","./test",0);
 	if(res == COMPILER_FAILED_WITH_ERROR){
+error:
 		printf("Compile failed or compile error\n");
 	}else if(res == COMPILER_FILE_COMPILE_OK){
 		printf("Everything compile fine\n");
