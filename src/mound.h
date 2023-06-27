96ba01aac420389c0f7016a0fc09c2f24143bead
CodeMaster114514 <128393278+CodeMaster114514@users.noreply.github.com>
2023-06-27 22:18:48 +0800

    Save mound.h.

--- src/mound.h
+++ src/mound.h
@@ -6,8 +6,8 @@ typedef struct{
     size_t write_p;
 } mound;
 
-mound* creat_mound(size_t len);
-mound* creat_mound_with_data(void* data,size_t len,size_t count);//调用后释放传入数据
+mound* create_mound(size_t len);
+mound* create_mound_with_data(void* data,size_t len,size_t count);//调用后释放传入数据
 void* read(mound* this,size_t p);
 void push(mound* this,void* data);
 void* pop(mound* this);
