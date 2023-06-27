023dc27918416b5737ea40fa9f326d4cf83f8bf0
CodeMaster114514 <128393278+CodeMaster114514@users.noreply.github.com>
2023-06-27 22:08:32 +0800

    Save lex_process.c.

--- src/lex_process.c
+++ src/lex_process.c
@@ -6,8 +6,7 @@ lex_process* lex_process_create(
 	void* private
 ){
 	lex_process* process = (lex_process*) calloc(1,sizeof(lex_process));
-	process->tokens = (Token*) calloc(1,sizeof(Token));
-	process->token_count = 0;
+	process->tokens = create_mound(sizeof(Token));
 	process->cprocess = cprocess;
 	process->private = private;
 	process->process_functions = lex_process_function;
@@ -23,28 +22,30 @@ void lex_process_free(lex_process* process){
 		free_buffer(process->expression.buffer_info[i]);
 	}
 	free(process->expression.buffer_info);
-	for(unsigned long long i = 0;i < process->token_count;++i)
+	for(size_t i = 0;i < process->tokens;++i)
 	{
-		if(process->tokens[i].type
+	    Token* the = read(process->tokens,i);
+		if(the->type
 		==
 		TOKEN_TYPE_STRING
 		||
-		process->tokens[i].type
+		the->type
 		==
 		TOKEN_TYPE_OPERATOR
 		||
-		process->tokens[i].type
+		the->type
 		==
 		TOKEN_TYPE_KEYWORDS
 		||
-		process->tokens[i].type
+		the->type
 		==
 		TOKEN_TYPE_COMMENT)
 		{
 			free(process->tokens[i].sval);
 		}
+		free(the);
 	}
-	free(process->tokens);
+	free_mound(process->tokens);
 	if(!process->private)
 	free(process->private);
 	free(process);
